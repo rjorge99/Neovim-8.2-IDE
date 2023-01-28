7dd9e0dbb0dad4cc9e9bab61f4dc186f035cfcba
Jorge Reyes <rjorge99@hotmail.com>
2023-01-27 22:25:20 -0600

    Se deshabilita color en <> en html

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -36,7 +36,7 @@ treesitter.setup({
 	auto_install = true,
 	rainbow = {
 		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		disable = { "html" },
 		extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
 		colors = {
